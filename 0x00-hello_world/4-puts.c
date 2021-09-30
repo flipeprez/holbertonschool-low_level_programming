0ff69892950509f9388517ba1b1eef3d2cea43db
flipeprez <flipeprez@gmail.com>
2021-09-30 11:26:05 -0700

    v1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*first exercise c */
-int main()
+int main(void)
 	{	
-		puts("programming is like building a multilingual puzzle\n");
-		return 0 ; 
+		puts("programming is like building a multilingual puzzle""\n");
+		return (0); 
 	}	
 
