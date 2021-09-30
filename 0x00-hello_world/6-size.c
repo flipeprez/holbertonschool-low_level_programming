5abd077d45f43ed2a4046d133d5aebf9193ba51a
flipeprez <flipeprez@gmail.com>
2021-09-30 12:57:18 -0700

    v1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,10 @@
 */
 int main(void)
 {
-		printf("size of a char is=%i byte\n", sizeof(char));
-		printf("size of a int is=%i byte\n", sizeof(int));
-		printf("size of a long int is=%i byte\n", sizeof(long int));
-		printf("size of a float is=%i byte\n", sizeof(float));
+		printf("size of a char is=%lu byte\n", sizeof(char));
+		printf("size of a int is=%lu byte\n", sizeof(int));
+		printf("size of a long int is=%lu byte\n", sizeof(long int));
+		printf("size of a long int is=%lu byte \n", sizeof(long long));
+		printf("size of a float is=%lu byte\n", sizeof(float));
 		return (0);
 }
