49e2524410f1ab7b21929fe91c6e8fbf1eaeed3f
flipeprez <flipeprez@gmail.com>
2021-09-30 12:22:22 -0700

    v1

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* * main -first c
+* * Return: 0
+*/
+int main(void)
+{
+		printf("size of a char is=%i byte\n", sizeof(char));
+		printf("size of a int is=%i byte\n", sizeof(int));
+		printf("size of a long int is=%i byte\n", sizeof(long int));
+		printf("size of a float is=%i byte\n", sizeof(float));
+		return (0);
+}
