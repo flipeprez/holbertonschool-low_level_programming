d473a1dce0a1850180ef0749eed94af37c5981e2
flipeprez <flipeprez@gmail.com>
2021-10-01 14:18:45 -0700

    v1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* * main -putchar
+* * Return: 0
+ */
+
+int main(void)
+{
+		int ch;
+
+		for (ch = 'z'; ch <= 'a'; ch--)
+		putchar(ch);
+		putchar('\n');
+		return (0);
+}
