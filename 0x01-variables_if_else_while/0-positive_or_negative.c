4744f46b44dc11b7e8bdd53b1441d0f48be99271
flipeprez <flipeprez@gmail.com>
2021-10-01 09:31:19 -0700

    v1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,10 +11,10 @@ int main(void)
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
 					if (n > 0)
-					printf("is positive");
+					printf("is positive" ,n);
 					if (n == 0)
-					printf("is zero");
+					printf("is zero" ,n);
 					if (n < 0)
-					printf("is negative");
+					printf("is negative" ,n);
 					return (0);
 }
