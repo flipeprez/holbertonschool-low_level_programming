5800294ee11c1edb2aeba893ea23076542d92b08
flipeprez <flipeprez@gmail.com>
2021-10-05 14:18:19 -0700

    v1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign -print the sign-
+ * Return: sign
+ *@n: integer
+ */
+
+int print_sign(int n)
+
+{
+		if (n > 0)
+		{
+			_putchar(43);
+			return (1);
+		}
+		else
+		{
+			if (n == 0)
+			{
+			_putchar(48);
+			return (0);
+			}
+			else
+			{
+			_putchar(45);
+			return (-1);
+			}
+		}
+}
