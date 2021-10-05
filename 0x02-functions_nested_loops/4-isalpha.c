0c6984fd5f3490a1c88913aa478029e5c2010337
flipeprez <flipeprez@gmail.com>
2021-10-05 12:54:40 -0700

    to_uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isalpha -show touppercase_case-
+ *@c: variable name.
+ *Return: 1
+ */
+
+int _isalpha(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
