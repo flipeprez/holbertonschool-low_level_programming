bb3c39f5dc27a2bcaaa68ec17adca693705f0b8c
flipeprez <flipeprez@gmail.com>
2021-10-05 16:27:22 -0700

    v1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,10 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		_putchar((n % 10) * (-1) + 48);
-		return ((n % 10) * (-1) + 48);
+		return ((n % 10) * (-1));
 	}
 		else
 		_putchar((n % 10) + 48);
-		return ((n % 10) + 48);
+		return (n % 10);
 
 }
