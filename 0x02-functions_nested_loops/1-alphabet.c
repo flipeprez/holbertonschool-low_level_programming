16573b7cc9961f144f0e1660417141cc74089548
flipeprez <flipeprez@gmail.com>
2021-10-05 10:07:35 -0700

    print alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet -print alphabet-
+ *Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
