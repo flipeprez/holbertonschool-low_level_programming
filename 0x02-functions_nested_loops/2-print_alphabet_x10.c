636f9d75c6e6ff5b61e3668313a13b6ba56738f1
flipeprez <flipeprez@gmail.com>
2021-10-05 11:15:29 -0700

    print 10 alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 -print 10 alphabet-
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	int alph;
+
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
