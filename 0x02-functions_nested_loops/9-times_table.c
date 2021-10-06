a8bb5c85414b753f33d958fa9ad27f2e36d402a3
flipeprez <flipeprez@gmail.com>
2021-10-06 11:00:55 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *times_table -print 9 table-
+ *
+ *Return: 0
+ */
+
+void times_table(void)
+
+{
+		int a;
+		int b;
+		int c;
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+		c = (a * b);
+		if (c < 10)
+		{
+			_putchar(' ');
+		} else
+		_putchar((c / 10) + '0');
+		_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
