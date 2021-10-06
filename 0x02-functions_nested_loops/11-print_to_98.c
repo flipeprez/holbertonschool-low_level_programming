a5d9ca47a90cfb63d5df15a0c8897e4fbe62a116
flipeprez <flipeprez@gmail.com>
2021-10-06 13:18:06 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - add function -
+ *@n:variable name
+ *Return: c
+ */
+
+void print_to_98(int n)
+
+{
+	for (n = 0; n < 99; n++)
+	{
+		if (n > 0)
+		{
+		printf("%d", n);
+		}
+		else if (n < 0)
+		{
+		printf("%d", n);
+		}
+		else
+		{
+		printf("%d", n);
+		}
+	}
+}
