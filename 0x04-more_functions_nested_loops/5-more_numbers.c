7cbc1099cf24024944d7cdd6a246453c1894be65
flipeprez <flipeprez@gmail.com>
2021-10-07 10:51:24 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,25 @@
 #include "main.h"
 /**
- *-more_numbers - print to 0 to 14-
- *@n:variable name
- *Return: c
+ *more_numbers - print to 0 to 14-
+ *
+ *Return: void
  */
 
 void more_numbers(void)
 
-{
-	for (i 
+{	int n;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+		}
+		_putchar(10);
+	}
 }
