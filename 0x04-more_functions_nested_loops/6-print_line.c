f1508ac17b989ae5b9881d8c92d6c9201820b750
flipeprez <flipeprez@gmail.com>
2021-10-07 11:30:57 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_line -print n _-
+ *
+ *@n: variable name.
+ *Return: void
+ */
+
+void print_line(int n)
+
+{
+	for (n = 0; n; n++)
+	{
+		if (n > 0)
+		{
+		_putchar(95);
+		}
+		_putchar('\n');
+	}
+
+}
