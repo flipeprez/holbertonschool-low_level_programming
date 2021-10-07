a6d1887f0c824293214be6ba86909818418e84c7
flipeprez <flipeprez@gmail.com>
2021-10-07 14:14:52 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *print_diagonal- print n \-
+ *
+ *@n: variable name.
+ *Return: void
+ */
+
+void print_diagonal(int n)
+
+{	int i, m;
+
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+	for (m = 0; m < n; m++)
+	{
+		for (i = 0; i < m; i++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+
+		_putchar(10);
+	}
+	}
+}
+
