cba4a9bed353f7a5531e4f7dd6a343b0c476ede0
flipeprez <flipeprez@gmail.com>
2021-10-07 09:57:14 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -15,6 +15,7 @@ void print_numbers(void)
 	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
-		_putchar(10);
 	}
+		_putchar(10);
+
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *-more_numbers - print to 0 to 14-
+ *@n:variable name
+ *Return: c
+ */
+
+void more_numbers(void)
+
+{
+	for (i 
+}
