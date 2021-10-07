d1bff7a7eecdb61a6de6ae1e85f1f6afe4a36905
flipeprez <flipeprez@gmail.com>
2021-10-07 06:47:46 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *mul -show the multiplication-
+ *@a: variable name.
+ *@b: variable name.
+ *Return: c
+ */
+
+int mul(int a, int b)
+
+{
+	int c;
+
+	c = (a * b);
+	return (c);
+}
