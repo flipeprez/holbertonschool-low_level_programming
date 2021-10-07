58e0329faa409ca83cc14e29fa8ab72d21589400
flipeprez <flipeprez@gmail.com>
2021-10-07 07:24:51 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers -print number between 0 and 9-
+ *
+ *
+ *Return: print number
+ */
+
+void print_numbers(void)
+
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+		_putchar(10);
+	}
+}
