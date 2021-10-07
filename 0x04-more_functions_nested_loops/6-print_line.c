d7e23de982abf00c5755e7816f72af05f5a9e4c9
flipeprez <flipeprez@gmail.com>
2021-10-07 11:49:15 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,16 @@
 
 void print_line(int n)
 
-{
-	for (n = 0; n; n++)
+{	int i;
+
+	for (i = 0; i < n; i++)
 	{
 		if (n > 0)
 		{
-		_putchar(95);
+		_putchar('_');
 		}
-		_putchar('\n');
 	}
+	_putchar(10);
+
 
 }
