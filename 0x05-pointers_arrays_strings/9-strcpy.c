51bed7c18147a56b7c42694fa481ca1c5ea33bb6
flipeprez <flipeprez@gmail.com>
2021-10-13 18:35:15 -0700

    function that copies the string pointed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,14 @@
  */
 
 char *_strcpy(char *dest, char *src)
-
+{
 	int n;
-	for(n = 0; src[n] != 0; n++)
+
+	for (n = 0; src[n] != 0; n++)
 	{
 		dest[n] = src[n];
-	}	
+	}
 	dest[n++] = 0;
-	return(dest)
+
+	return (dest);
+}
