deeb271b253391d3dea45262be90bb11ca569320
flipeprez <flipeprez@gmail.com>
2021-10-26 14:59:08 -0700

    return 2 dimentional pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,15 +28,15 @@ int **alloc_grid(int width, int height)
 		p[i] = malloc(sizeof(int) * width);
 
 		if (p[i] == 0)
-
-		for (; i >= 0; i--)
 		{
-			free(p[i]);
+			for (; i >= 0; i--)
+			{
+				free(p[i]);
+			}
+
+			free(p);
+			return (0);
 		}
-
-		free(p);
-		return (0);
-
 
 	}
 
