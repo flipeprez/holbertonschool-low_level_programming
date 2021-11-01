09d25b8922c2b313c599e8808fc0613c45586b51
flipeprez <flipeprez@gmail.com>
2021-11-01 15:31:57 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - check the code
+ *@d:varable name.
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
