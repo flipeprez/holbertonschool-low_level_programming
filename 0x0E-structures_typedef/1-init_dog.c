98de66ef29bb78ecf2e894558203942f952035d7
flipeprez <flipeprez@gmail.com>
2021-11-01 10:29:02 -0700

    structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - function dog
+ * dog - initialize dog.
+ * @dog:variable name.
+ * @d:name variable.
+ * @age:variable name.
+ * @owner:variable name.
+ * @name:variable name.
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+	int main(void)
+	{
+	struct dog dg;
+	}
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+Struct dog - define a struct dog
+ * @a: First member
+ * @b: Second member
+ * @c: Third member
+ *
+ * 
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
