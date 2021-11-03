2f2f2807d46ceb2b00c289e4525e010fe1fc01ea
flipeprez <flipeprez@gmail.com>
2021-11-03 16:53:46 -0700

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,6 +14,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	printf("%d\n", get_op_func(argv[1])(atoi(argv[2]), atoi(argv[3])));
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 		return (0);
 }
