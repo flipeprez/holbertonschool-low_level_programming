9cc257dd9ea87b0479e58ab3d2056db01ef735f5
flipeprez <flipeprez@gmail.com>
2021-11-03 16:35:50 -0700

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,39 +2,58 @@
 #include<stdlib.h>
 /**
  * op_add - add function
- * op_sub - sub function
- * op_mul - product for a and b
- * op_div - division for a and b
- * op_mod - module function
  * @a: int value
  * @b: int value
+ * Return:int
  */
 
 int op_add(int a, int b)
 {
 	return ((a + b));
 }
+/**
+ * op_sub - subfunction
+ * @a:int value
+ * @b:int value
+ * Return:int
+ */
 int op_sub(int a, int b)
 {
 	return ((a - b));
 }
+/**
+ * op_mul - product a and b
+ * @a:int value
+ * @b:int value
+ * Return:int
+ */
 int op_mul(int a, int b)
 {
 	return ((a * b));
 }
+/**
+ * op_div -division for a and b
+ * @a:int value
+ * @b:int value
+ * Return:int
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
 		return ((a / b));
-	else
-		printf("error\n");
-		exit(100);
+	printf("error\n");
+	exit(100);
 }
+/**
+ * op_mod -module
+ * @a:int value
+ * @b:int value
+ * Return:int
+ */
 int op_mod(int a, int b)
 {
 	if (b == 0)
 		return ((a % b));
-	else
-		printf("error\n");
-		exit(100);
+	printf("error\n");
+	exit(100);
 }
