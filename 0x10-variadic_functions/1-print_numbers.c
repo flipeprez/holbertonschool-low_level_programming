1635e48967cdf6ad826092cb3fd7b5414baa0a4a
flipeprez <flipeprez@gmail.com>
2021-11-04 11:04:05 -0700

    prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,14 +16,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list li;
 	unsigned int i;
 
-	if (separator == 0)
-		separator = "";
 	va_start(li, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(li, unsigned int));
-		printf("%s", separator + 2 - 1);
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(li);
