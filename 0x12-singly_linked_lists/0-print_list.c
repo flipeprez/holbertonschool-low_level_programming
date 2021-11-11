9874f3729250edd1b1b2ed41bf9301a8581c5227
flipeprez <flipeprez@gmail.com>
2021-11-11 11:20:02 -0800

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,18 +15,15 @@ size_t print_list(const list_t *h)
 {
 	int node = 0;
 
-	node++;
-
-	if (!h)
-		return (0);
 	if (h)
 	{
+		node++;
 	if (h->str == NULL)
-		printf("[0] (nil)");
+		printf("[0](nil)\n");
 
 	else
-	printf("[%d %s\n]", h->len, h->str);
-	h = h->next;
+	printf("[%d] %s\n", h->len, h->str);
+	node += print_list(h->next);
 	}
 	return (node);
 }
