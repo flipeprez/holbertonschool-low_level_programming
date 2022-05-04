cf88355f2ab52091a7f9f1407aab446a56f00774
flipeprez <flipeprez@gmail.com>
2022-05-04 13:38:31 -0700

    solve problems between vs and linux

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,26 +1,26 @@
 #include "search_algos.h"
 
+
 /**
  * linear_search - look for a value in an array.
  * @array: list of integers.
  * @size: size of the array.
  * @value: value that the algorithm try to find.
  * Return: index in which value is located in the array.
- * if value is not found return -1.
+ * if value is not found return -1
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-    size_t = indx;
+	size_t indx;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (indx = 0; indx < size - 1; indx++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", indx, array[indx]);
-        if (array[idx] == value)
-            return (idx);
-    }
-    return (-1);
+	for (indx = 0; indx < size - 1; indx++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", indx, array[indx]);
+		if (array[indx] == value)
+			return (indx);
+	}
+		return (-1);
 }
